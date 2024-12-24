a41d9f2be1a46358a367f6ebbf7048c7213aea85
umaidashi <umaidashi18@gmail.com>
2024-12-24 13:03:27 +0900

    feat: event

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -2,9 +2,15 @@
   export let task;
 
   export let completed = false;
+
+  function handleInput(event) {
+    if (event.target.checked) {
+      alert("Good job!");
+    }
+  }
 </script>
 
 <p>
-  <input type="checkbox" checked={completed} />
+  <input type="checkbox" checked={completed} on:input={handleInput} />
   {task}
 </p>
